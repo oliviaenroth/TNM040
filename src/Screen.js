0547c3b0f9853a1ec7135f40ca6489aa5b4aab29
Kevin Evans <kevev243@student.liu.se>
2017-11-03 11:31:08 +0100

    created react app

---
+++ src/Screen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Screen.css';
+
+class App extends Component {
+	render(){
+		return(
+			<div id="screen">
+
+			</div>
+		)
+	}
+}
+
+export default Screen;
